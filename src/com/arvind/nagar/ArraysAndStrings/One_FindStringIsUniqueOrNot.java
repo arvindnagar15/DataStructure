8ac0efd1a564fc5e64277b839b3a7eb32ed4891c
Scorpio <arvindnagar1511@gmail.com>
2019-04-30 06:31:16 -0400

    update

--- src/com/arvind/nagar/ArraysAndStrings/One_FindStringIsUniqueOrNot.java
+++ src/com/arvind/nagar/ArraysAndStrings/One_FindStringIsUniqueOrNot.java
@@ -10,7 +10,7 @@ public class One_FindStringIsUniqueOrNot {
 	public static void main(String[] args) {
 		try {
 			String str = "Arvinda12";
-			System.out.println("Unique : "+isUnique(str));
+			System.out.println("Unique String : "+isUnique(str));
 
 			//System.out.println(Arrays.toString(ch));
 		}catch(Exception e) {
