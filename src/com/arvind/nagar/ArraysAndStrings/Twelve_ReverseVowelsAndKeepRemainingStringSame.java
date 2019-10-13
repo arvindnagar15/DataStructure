4fbef77994212e06da977cb115f522ad58250fb4
unknown <arvindnagar1511@gmail.com>
2019-10-13 09:09:45 -0400

    delete

---
+++ src/com/arvind/nagar/ArraysAndStrings/Twelve_ReverseVowelsAndKeepRemainingStringSame.java
@@ -0,0 +1,77 @@
+package com.arvind.nagar.ArraysAndStrings;
+
+import java.util.Arrays;
+
+import com.oracle.webservices.internal.api.databinding.Databinding.Builder;
+
+import jdk.nashorn.internal.runtime.arrays.NumericElements;
+
+/**
+ * 
+ * @author Scorpio
+ *	
+ *Reverse vowels, but keep string same - 
+ *
+ */
+public class Twelve_ReverseVowelsAndKeepRemainingStringSame {
+
+	public static void main(String[] args) {
+		try {
+			String s = "Hello friends";
+			char[] vowelArray = new char[s.length()];
+			char[] strToCharArray = s.toCharArray();
+			
+			if(s == null || s.equals(" ") || s.length() <= 1) {
+				System.out.println("Strning is empty or having just one element");
+				return;
+			}
+			int numberOfVowels = 0;
+			for(int i = 0; i< strToCharArray.length; i++) {
+				if(isVowels(String.valueOf(strToCharArray[i]))) {
+					vowelArray[numberOfVowels] = strToCharArray[i];
+					numberOfVowels++;
+				}
+			}
+			char[] newVowelArray = Arrays.copyOfRange(vowelArray, 0, numberOfVowels);
+			if(numberOfVowels <= 1) {
+				System.out.println("Number of vowels are : "+numberOfVowels);
+				return;
+			}
+			if(numberOfVowels > 1) { // do nothing if no of vowels not greater than 1
+				
+				StringBuilder builder = new StringBuilder();
+				int counter = 0;
+				for(int i = 0; i< strToCharArray.length; i++) {
+					if(isVowels(String.valueOf(strToCharArray[i]))) {
+						builder.append(String.valueOf(newVowelArray[newVowelArray.length -1 - counter]));
+						counter++;
+					}else {
+						builder.append(String.valueOf(strToCharArray[i]));
+					}
+				}
+				System.out.println(builder.toString());
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	private static boolean isVowels(String s) {
+		switch(s) {
+		case "a":
+			return true;
+		case "e":
+			return true;
+		case "i":
+			return true;
+		case "o":
+			return true;
+		case "u":
+			return true;
+		}
+		return false;
+	}
+
+}
